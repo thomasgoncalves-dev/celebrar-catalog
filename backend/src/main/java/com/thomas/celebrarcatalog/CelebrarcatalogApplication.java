039d33bfacdedb121da5997b354d7385548453cf
Thomás Gonçalves <thomasgoncalves.dev@gmail.com>
2026-09-10 19:32:38 -0300

    chore: estrutura inicial do backend Spring Boot

---
+++ backend/src/main/java/com/thomas/celebrarcatalog/CelebrarcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.thomas.celebrarcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebrarcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebrarcatalogApplication.class, args);
+	}
+
+}
